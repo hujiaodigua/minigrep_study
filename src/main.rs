82e3dc2ce7f13d2908c53baa40350e1695244a18
hujiaodigua <371629179@qq.com>
2019-07-11 16:31:24 +0800

    eprintln!

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main()
     // let config = parse_config(&args);
     // let config = Config::new(&args);
     let config = Config::new(&args).unwrap_or_else(|err|{
-        println!("Problem parsing arguments: {}", err);    // 这种检查错误的方式还是很精妙的
+        eprintln!("Problem parsing arguments: {}", err);    // 这种检查错误的方式还是很精妙的
         process::exit(1);
     });
 
@@ -21,7 +21,7 @@ fn main()
     println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(config){    // minigrep是工程名称,run是库lib.rs中的pub fn
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         
         process::exit(1);
     }
